eb5464ed57d5b5e7da617edfec60d6253e3e3391
Tu Dinh <tu.dinh@metropolia.fi>
2024-09-04 07:48:49 +0300

    feature: navigation bar without styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,72 @@
+import logo from "../images/logo.svg";
+
 const Navbar = () => {
   return (
     <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
+      <nav classname="navbar">
+        <div classname="nav-center">
+          <div classname="nav-header">
+            <img src={logo} classname="nav-logo" alt="backroads" />
+            <button type="button" classname="nav-toggle" id="nav-toggle">
+              <i classname="fas fa-bars" />
+            </button>
+          </div>
+          <ul classname="nav-links" id="nav-links">
+            <li>
+              <a href="#home" classname="nav-link">
+                home
+              </a>
+            </li>
+            <li>
+              <a href="#about" classname="nav-link">
+                about
+              </a>
+            </li>
+            <li>
+              <a href="#services" classname="nav-link">
+                services
+              </a>
+            </li>
+            <li>
+              <a href="#tours" classname="nav-link">
+                tours
+              </a>
+            </li>
+          </ul>
+          <ul classname="nav-icons">
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                classname="nav-icon"
+              >
+                <i classname="fab fa-facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                classname="nav-icon"
+              >
+                <i classname="fab fa-twitter" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                classname="nav-icon"
+              >
+                <i classname="fab fa-squarespace" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
